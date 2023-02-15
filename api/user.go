223dea8f3ba6b8cee7b108ba69119a3317e62afa
chubru <ya@yandex.ru>
2023-02-15 20:05:31 +0300

    add swagger

--- api/user.go
+++ api/user.go
@@ -2,36 +2,45 @@ package api
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/session"
 	"net/http"
-	"web-control/connectors/connectors"
 )
 
-func User(app *fiber.App, store *session.Store, cn connectors.Connector) {
+type password struct {
+	Password string
+}
 
-	app.Post("/resetPassword", func(c *fiber.Ctx) error {
-		type newPassword struct {
-			Password string
-		}
-		var pass = newPassword{}
-		c.BodyParser(&pass)
+//	@Summary		Reset User Password
+//	@ID				reset-user-password
+//
+//	@Tags 			User
+//	@Produce		json
+//	@Param			account	body password true "password"
+//	@Router			/resetPassword [post]
+// @Success         200  {object}  string
+// @Failure         403  {object}  string
+// @Failure         500  {object}  string
+func (controller *abstractController) ResetUserPassword(c *fiber.Ctx) error {
+	var pass = password{}
+	c.BodyParser(&pass)
 
-		sess, err := store.Get(c)
+	sess, err := controller.store.Get(c)
 
-		if err != nil {
-			return c.Status(http.StatusBadRequest).JSON("Can not find cookie")
-		}
+	if err != nil {
+		return c.Status(http.StatusBadRequest).JSON("Can not find cookie")
+	}
 
-		ID, ok := sess.Get("id").(int)
+	ID, ok := sess.Get("id").(int)
 
-		if !ok {
-			return c.Status(http.StatusBadRequest).JSON("Can not find user ID")
-		}
+	if !ok {
+		return c.Status(http.StatusBadRequest).JSON("Can not find user ID")
+	}
 
-		if !cn.ResetUserPassword(ID, pass.Password) {
-			return c.Status(http.StatusBadRequest).JSON("Database error")
-		}
-		return c.JSON("ok")
-	})
+	if !controller.cn.ResetUserPassword(ID, pass.Password) {
+		return c.Status(http.StatusBadRequest).JSON("Database error")
+	}
+	return c.JSON("ok")
+}
+func initUserApi(app *fiber.App, controller *abstractController) {
+	app.Post("/resetPassword", controller.ResetUserPassword)
 	return
 }
